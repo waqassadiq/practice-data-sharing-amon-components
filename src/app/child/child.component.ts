b0726f0dbfa22d23f7fdba9629d95bd49490894b
Waqas <wakassadik@gmail.com>
2021-04-30 17:16:37 +0500

    parentName from parent transimitted to child component

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-child',
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ChildComponent implements OnInit {
 
   childName: String;
+  @Input() childParentName: string;
   constructor() { 
     this.childName = "John"
   }
