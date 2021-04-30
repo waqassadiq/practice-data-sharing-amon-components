1c97a89f88ea1e5a397159a34c0cb5cbe38895e3
Waqas <wakassadik@gmail.com>
2021-04-30 17:00:04 +0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'practice-data-sharing-amon-components';
+
+  parentName: String;
+
+  constructor(){
+    this.parentName = "Smith";
+  }
 }
---
+++ src/app/child/child.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-child',
+  templateUrl: './child.component.html',
+  styleUrls: ['./child.component.css']
+})
+export class ChildComponent implements OnInit {
+
+  childName: String;
+  constructor() { 
+    this.childName = "John"
+  }
+
+  ngOnInit(): void {
+  }
+
+}
